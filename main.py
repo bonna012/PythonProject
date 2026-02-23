864059340336f9eb2acaf9393998cb0702cee3e3
Asia Jaman Bonna <asiabonna012@gmail.com>
2026-02-23 20:06:14 +0100

    second commit
    While loop, try-except error handling, if-else condition

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("Hello GitHub!")
+name = input("What's your name?")
+print("Nice to meet you,",name)
+count = len(name)
+print("Your name has", count, "letters.")
+
+while True:
+    age = input("What's your age? ")
+    try:
+        age_num = float(age)
+        break
+    except ValueError:
+        print("Please enter a valid number for age.")
+
+if age_num <= 18:
+        print("You cannot drink alcohol.")
+else:
+        drink = input("What do you wanna drink?")
+        print("Enjoy your", drink + "!")
